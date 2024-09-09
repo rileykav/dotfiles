9db69177d61573347bbb1553cc7b1833036e4ff9
rileykav <lykavanagh1@gmail.com>
2024-09-09 21:22:56 +0100

    adding a few aliases to ps

--- powershell/alias.ps1
+++ powershell/alias.ps1
@@ -1,4 +1,11 @@
 # Alias Functions
 
-
+# Git Alias
 function gst {git status}
+
+
+
+# Config Files
+function psrc {vim $profile}
+function psalias {vim $home\.dotfiles\powershell\alias.ps1}
+function vimrc {vim $home\_vimrc}
