2886f7025072e3213a409e6d766c7019eddd0d74
rileykav <lykavanagh1@gmail.com>
2024-09-11 21:58:44 +0100

    adding some path descriptions for installing windows

--- install-files/windows-install.ps1
+++ install-files/windows-install.ps1
@@ -1,6 +1,13 @@
 # Powersherll Services
 Update-Help
 
+
+# Editing Path (items that sometimes do not get added automatically)
+# personal script folders
+# edge, okular, vim
+#
+# Maybe add these only to $profile
+
 # Winget
 winget install vim
 winget install MiKTeX
