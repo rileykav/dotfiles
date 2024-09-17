91ee15efcaea6ab15e45a94514bb0ac3908376d5
rileykav <65302238+rileykav@users.noreply.github.com>
2024-09-17 17:44:48 +0100

    Adding install instructions for mac

---
+++ install-files/mac-install.sh
@@ -0,0 +1,9 @@
+# Symlink Important rc files
+# ln -s /Path/to/RealFile /Path/to/Symlink
+ln -s $Dotfiles/vim/vimrc.symlink $HOME/.vimrc
+ln -s $Dotfiles/git/config.gitconfig $HOME/.gitconfig
+
+
+# Installing brew
+brew install macvim
+
