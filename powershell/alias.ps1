01ab65a62aa0e851cb5c42098f02e9f6a3671b31
rileykav <lykavanagh1@gmail.com>
2024-09-26 03:48:28 +0100

    fixing bug in vimbasic

--- powershell/alias.ps1
+++ powershell/alias.ps1
@@ -15,7 +15,7 @@ function vimvisuals {vim $dotfiles\vim\visuals.vim}
 function vimfiletypes {vim $dotfiles\vim\filetypes.vim}
 function vimwindows {vim $dotfiles\vim\windows.vim}
 function vimswapfiles {cd $dotfiles\vim\swapfiles}
-function vimbasic {vim -u $dotfiles\vim\basic.vim}
+function vimbasic ($file) {vim -u $dotfiles\vim\basic.vim $file}
 function vimbasicedit {vim $dotfiles\vim\basic.vim}
 
 function windowsinstall {vim $dotfiles\install-files\windows-install.ps1}
