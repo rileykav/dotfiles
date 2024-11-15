a262546171164dfefa6f38d41470b73d16d19b50
rileykav <65302238+rileykav@users.noreply.github.com>
2024-11-15 14:47:41 +0000

    Creating a macinstall file (merge with other one?)

---
+++ mac/macinstall.sh
@@ -0,0 +1,13 @@
+# Main way to install command line tools
+brew install vim
+brew install mkvtoolnix
+
+
+
+
+# Manual site downloads
+# https://pilotmoon.com/scrollreverser      # Allows independent reversal of mouse and trackpad scrolling
+
+
+
+
