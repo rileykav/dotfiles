0517bc634144226d415735f043c26442c7127546
rileykav <65302238+rileykav@users.noreply.github.com>
2024-12-05 16:55:37 +0000

    Adding an error check for tunes.js

--- tmux/tunes.js
+++ tmux/tunes.js
@@ -1,46 +1,58 @@
 let output = "";
 let dualoutput = "";
-if (Application("Spotify").running()) {
-    if (Application("Spotify").playerState() == "playing") {
-        const track = Application("Spotify").currentTrack;
-        const artist = track.artist();
-        const title = track.name();
-        output += `${title} - ${artist}`.substr(0, 100)
-        dualoutput = " & "
-    }
-}
-
-if (Application("Music").running()) {
-    if (Application("Music").playerState() == "playing") {
-        const track = Application("Music").currentTrack;
-        const artist = track.artist();
-        const title = track.name();
-        output += dualoutput ? " | " : ""; // Add a separator if needed
-        output += `${title} - ${artist}`.substr(0, 100)
-    }
-}
-if ( (Application("Safari").running()) || (Application("Plexamp").running())) {
-    if ( output=="" ){
-        try {
-            const app = Application.currentApplication();
-            app.includeStandardAdditions = true;
-            const title = app.doShellScript("nowplaying-cli get title");
-            const artist = app.doShellScript("nowplaying-cli get artist");
-            if (title != "" || artist != ""){
-                output += dualoutput ? " | " : ""; // Add a separator if needed
-                output += `${title} - ${artist}`.substr(0, 100)
-            }
-        } catch (e) {
-            // Handle errors in executing the shell command
-            output += " (Error running Zsh command)";
+try {
+    if (Application("Spotify").running()) {
+        if (Application("Spotify").playerState() == "playing") {
+            const track = Application("Spotify").currentTrack;
+            const artist = track.artist();
+            const title = track.name();
+            output += title + " - " + artist
+            dualoutput = " & "
         }
     }
-}
 
-if (output == ""){
-    output+= "Not Playing"
-} else {
-    output = output.substr(0,30)
-}
+    if (Application("Music").running()) {
+        if (Application("Music").playerState() == "playing") {
+            const track = Application("Music").currentTrack;
+            const artist = track.artist();
+            const title = track.name();
+            output += dualoutput ? " | " : ""; // Add a separator if needed
+            output += title + " - " + artist
+        }
+    }
+    if ( (Application("Safari").running()) || (Application("Plexamp").running())) {
+        if ( output=="" ){
+            try {
+                const app = Application.currentApplication();
+                app.includeStandardAdditions = true;
+                const title = app.doShellScript("nowplaying-cli get title");
+                const artist = app.doShellScript("nowplaying-cli get artist");
+                if (title == "" && artist == ""){
+                    // Do Nothing
+                } else if (artist == ""){
+                    output += dualoutput ? " | " : ""; // Add a separator if needed
+                    output += title
+                } else {
+                    output += dualoutput ? " | " : ""; // Add a separator if needed
+                    output += title + " - " + artist
+                }
 
+            } catch (e) {
+                // Handle errors in executing the shell command
+                output += " (Error running Zsh command)";
+            }
+        }
+    }
+
+    if (output == ""){
+        output+= "Not Playing"
+    } else {
+        output = output.substr(0,70)
+    }
+
+} catch (error) {
+    console.log("Unexpected Error:", error);
+    output = "Error Occured";
+        }
 output;
+
