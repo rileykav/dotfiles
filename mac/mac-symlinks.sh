072527549719889bcff4dedbe11ca43cc27139cc
Riley Kavanagh <lykavanagh1@gamil.com>
2025-03-15 12:24:38 +0000

    adding symlink creator

---
+++ mac/mac-symlinks.sh
@@ -0,0 +1,7 @@
+
+
+
+
+
+# BakkesMod Config file
+ln -s ~/.dotfiles/games/bakkesmod/config.cfg '/Users/riley/Library/Application Support/CrossOver/Bottles/Steam/drive_c/users/crossover/AppData/Roaming/bakkesmod/bakkesmod/cfg/config.cfg'
