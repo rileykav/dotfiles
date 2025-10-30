42ebed88c72a4549bd3426aaef3d55f5b8fccaa6
Riley Kavanagh <lykavanagh1@gmail.com>
2025-10-30 15:44:37 +0000

    vim: YCM settings for pylsp

--- vim/ycm_global_extra_cong.py
+++ vim/ycm_global_extra_cong.py
@@ -1,6 +1,22 @@
-def Settings( **kwargs ):
-  client_data = kwargs[ 'client_data' ]
-  return {
-    'interpreter_path': client_data[ 'g:ycm_python_interpreter_path' ],
-    'sys_path': client_data[ 'g:ycm_python_sys_path' ]
-  }
+def Settings(**kwargs):
+    client_data = kwargs['client_data']
+    return {
+        'interpreter_path': client_data['g:ycm_python_interpreter_path'],
+        'sys_path': client_data['g:ycm_python_sys_path'],
+        'ls': {
+            'pylsp': {
+                'plugins': {
+                    'pyflakes': {'enabled': False},
+                    'flake8': {
+                        'enabled': True,
+                        'ignore': [
+                            'E226',  # missing whitespace around arithmetic operator
+                            # 'E203',  # whitespace before ‘,’, ‘;’, or ‘:’
+                            'E303',  # too many blank lines (3)
+                            'F401',  # module imported but not used
+                        ],  # Example: ignore line length + line break warnings
+                    }
+                }
+            }
+        }
+    }
