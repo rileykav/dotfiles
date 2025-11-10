c4e4803154c15ad88705954a348b90821ad5a328
Riley Kavanagh <lykavanagh1@gmail.com>
2025-11-10 14:59:30 +0000

    zshfunctions: adding an extra drive to (un)mount

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -907,6 +907,7 @@ function mount-desktop() {
     osascript -e 'mount volume "smb://lykav@rileys-desktop/Users"'
     osascript -e 'mount volume "smb://lykav@rileys-desktop/Seagate 4TB"'
     osascript -e 'mount volume "smb://lykav@rileys-desktop/Seagate HDD (E)"'
+    osascript -e 'mount volume "smb://lykav@rileys-desktop/Crucial Sata SSD (D)"'
 #     osascript -e 'mount volume "smb://lykav@rileys-desktop/Ext-SSD 1TB"'
 }
 function unmount-desktop() {
@@ -914,6 +915,7 @@ function unmount-desktop() {
     diskutil unmount "/Volumes/Seagate HDD (E)"
 #     diskutil unmount "/Volumes/Ext-SSD 1TB"
     diskutil unmount "/Volumes/Users"
+    diskutil unmount "/Volumes/Crucial Sata SSD (D)"
 }
 
 
